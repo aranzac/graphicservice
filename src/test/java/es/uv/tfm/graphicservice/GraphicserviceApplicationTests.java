ec3c043910dd24739de01d4c8fcf69d054f92f56
aranzac <aranza@PC.localdomain>
2020-11-05 12:54:28 +0000

    PRimera

---
+++ src/test/java/es/uv/tfm/graphicservice/GraphicserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uv.tfm.graphicservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
